10f93a6b05825802829be6a16377c948786ec75b
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2019-11-06 14:36:35 -0600

    added questions for 19.2 and 19.3

--- Ch19/src/ch19Exercise2/Ch19Exercise2Main.java
+++ Ch19/src/ch19Exercise2/Ch19Exercise2Main.java
@@ -1,5 +1,13 @@
 package ch19Exercise2;
 
 public class Ch19Exercise2Main {
+	/* 
+	 * (Implement GenericStack using inheritance)
+	 * In Listing 19.1, GenericStack is implemented using composition. 
+	 * Define a new stack class that extends ArrayList.
+	 * Draw the UML diagram for the classes and then implement GenericStack.
+	 * Write a test program that prompts the user to 
+	 * enter five strings and displays them in reverse order.
+	 */
 
 }
--- Ch19/src/ch19Exercise3/Ch19Exercise3Main.java
+++ Ch19/src/ch19Exercise3/Ch19Exercise3Main.java
@@ -1,5 +1,12 @@
 package ch19Exercise3;
 
 public class Ch19Exercise3Main {
+	/* 
+	 * (Distinct elements in ArrayList) 
+	 * Write the following method that returns a new ArrayList. 
+	 * The new list contains the non-duplicate elements from the original list.
+	 * 
+	 * public static <E> ArrayList<E> removeDuplicates(ArrayList<E> list)
+	 */
 
 }
